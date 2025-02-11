519a4438671b4e6c3d5b0fa31606ed61e9b5f436
zeppombal <79043513+zeppombal@users.noreply.github.com>
2025-02-11 15:09:51 +0000

    Create run.py

---
+++ run.py
@@ -0,0 +1,68 @@
+import argparse
+import subprocess
+
+import pandas as pd
+from vllm import LLM, SamplingParams
+
+DEFAULT_JUDGE = "gpt-4-1106-preview"
+
+ANSWER_MODEL_TEMPLATE = """bench_name: arena-hard-v0.1
+temperature: 0.0
+max_tokens: 4096
+num_choices: 1
+
+
+model_list:
+  - {model}"""
+
+
+def turns_to_messages(turns):
+    content = turns[0]["content"]
+    message = [{"role": "user", "content": content}]
+    return message
+
+
+def main(answer_model, judge_model):
+    if judge_model != DEFAULT_JUDGE:
+        raise NotImplementedError("Only gpt-4-1106-preview is supported as judge model")
+    model_name = answer_model.replace("/", "__")
+    # Get answers
+    questions = pd.read_json("data/arena-hard-v0.1/question.jsonl", lines=True)
+    messages = questions["turns"].apply(turns_to_messages).tolist()
+    model = LLM(answer_model)
+    sampling_params = SamplingParams(
+        temperature=0.0, max_tokens=4096, gpu_memory_utilization=0.2
+    )
+    model_output = model.chat(messages, sampling_params, use_tqdm=True)
+    answers = [output.outputs[0].text for output in model_output]
+    # Save answers
+    existing_answers = pd.read_json(
+        "data/arena-hard-v0.1/model_answer/gpt-4-0314.jsonl", lines=True
+    )
+    turns_to_save = [
+        [{"index": 0, "turns": [{"content": answer}]}] for answer in answers
+    ]
+    existing_answers["choices"] = turns_to_save
+    existing_answers["answer_id"] = range(len(answers))
+    existing_answers["model_id"] = model_name
+    existing_answers["token_len"] = [len(answer) for answer in answers]
+    existing_answers.to_json(
+        f"data/arena-hard-v0.1/model_answer/{model_name}.jsonl",
+        lines=True,
+        orient="records",
+        force_ascii=False,
+    )
+    # Add answer model
+    answer_config = ANSWER_MODEL_TEMPLATE.format(model=model_name)
+    with open("config/arena-hard-v0.1/gen_answer_config.yaml", "w") as f:
+        f.write(answer_config)
+    # Run judge with the bash command python gen_judgment.py
+    subprocess.run(["python", "gen_judgment.py"])
+    subprocess.run(["python", "show_result.py"])
+
+
+parser = argparse.ArgumentParser(description="Wrapper for Arena Hard Auto")
+parser.add_argument("--answer_model", required=True)
+parser.add_argument("--judge_model", default="gpt-4-1106-preview")
+args = parser.parse_args()
+main(args.answer_model, args.judge_model)
